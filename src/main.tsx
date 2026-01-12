047df3ed9481c7a623aeba8fb7d182937f20ee1c
Harrycarter555 <top9pdf@gmail.com>
2026-01-13 03:24:36 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
-// src/main.tsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'  // Yeh line honi chahiye
+import App from './App'
+import './index.css'  // Make sure this line exists
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
