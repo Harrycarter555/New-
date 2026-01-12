b195d32e2e04057e2748b0bd7361a749a4c47574
Harrycarter555 <top9pdf@gmail.com>
2026-01-12 15:26:28 +0530

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Tumhare Firebase project ke config se replace kar dena
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyD3AGmLFFuUkIzpW0O3YyrgBbkhe98f-wg",
+  authDomain: "reelearn-505d9.firebaseapp.com",
+  projectId: "reelearn-505d9",
+  storageBucket: "reelearn-505d9.appspot.com",
+  messagingSenderId: "207249486424",
+  appId: "1:207249486424:web:63461de258102164f8102d"
 };
 
 // Initialize Firebase
