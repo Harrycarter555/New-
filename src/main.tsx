3cd91c11a3584121bdebb5ae7af2486651511832
Harrycarter555 <top9pdf@gmail.com>
2026-01-12 20:26:17 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,11 @@
 // src/main.tsx
-import './index.css';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-// Global styles + Tailwind directives yahan import hote hain
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'  // Yeh line honi chahiye
 
-// Root element ko safely grab karte hain (null check ke saath)
-const rootElement = document.getElementById('root');
-
-if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-} else {
-  // Agar root nahi mila toh error console mein dikhao (development ke liye helpful)
-  console.error('Root element not found! Check index.html for <div id="root"></div>');
-}
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
