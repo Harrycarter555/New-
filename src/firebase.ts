4a6131aa7f358118f37ce3219ccb2905bf1c39d1
Harrycarter555 <top9pdf@gmail.com>
2026-01-12 18:47:56 +0530

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,14 +1,15 @@
-import { getApps, initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+// src/firebase.ts
+import { initializeApp, getApps } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD3AGmLFFuUkIzpW0O3YyrgBbkhe98f-wg",
-  authDomain: "reelearn-505d9.firebaseapp.com",
-  projectId: "reelearn-505d9",
-  storageBucket: "reelearn-505d9.appspot.com",
-  messagingSenderId: "207249486424",
-  appId: "1:207249486424:web:63461de258102164f8102d"
+  apiKey: 'AIzaSyD3AGmLFFuUkIzpW0O3YyrgBbkhe98f-wg',
+  authDomain: 'reelearn-505d9.firebaseapp.com',
+  projectId: 'reelearn-505d9',
+  storageBucket: 'reelearn-505d9.appspot.com',
+  messagingSenderId: '207249486424',
+  appId: '1:207249486424:web:63461de258102164f8102d',
 };
 
 const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
