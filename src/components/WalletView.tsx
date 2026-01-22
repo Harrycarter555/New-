65e6fb56dee28bbb52ba4e98086a0c969f3bd985
Harrycarter555 <top9pdf@gmail.com>
2026-01-23 00:39:47 +0530

    Add missing import statement in WalletView component

--- src/components/WalletView.tsx
+++ src/components/WalletView.tsx
@@ -1,3 +1,4 @@
+//src/components/WalletView.tsx
 import React, { useState, useEffect } from 'react';
 import { User, AppState, PayoutStatus, Platform, SubmissionStatus, Campaign } from '../types';
 import { ICONS } from '../constants';
