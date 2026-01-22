bcd5ff39c666093087c82ea72f37d766f991086e
Harrycarter555 <top9pdf@gmail.com>
2026-01-23 00:40:38 +0530

    Add import for GoogleGenerativeAI component

--- src/components/VerifyView.tsx
+++ src/components/VerifyView.tsx
@@ -1,3 +1,4 @@
+//src/components/VerifyView.tsx
 import React, { useState, useEffect } from 'react';
 import { AppState, User, Platform, SubmissionStatus, Campaign } from '../types';
 import { GoogleGenerativeAI } from "@google/generative-ai";
