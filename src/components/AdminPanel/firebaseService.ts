c4f727da48738b4c2188c466923ff84bf2027652
Harrycarter555 <top9pdf@gmail.com>
2026-02-01 01:07:56 +0530

    Update firebaseService.ts

--- src/components/AdminPanel/firebaseService.ts
+++ src/components/AdminPanel/firebaseService.ts
@@ -83,7 +83,7 @@ export const initializationService = {
         endDate.setFullYear(today.getFullYear() + 1);
         
         batch.set(cashflowRef, {
-          dailyLimit:0,
+          dailyLimit:'',
           todaySpent: 0,
           startDate: today.toISOString().split('T')[0],
           endDate: endDate.toISOString().split('T')[0],
@@ -101,7 +101,7 @@ export const initializationService = {
       if (!configSnap.exists()) {
         batch.set(configRef, {
           minWithdrawal: 100,
-          dailyLimit:0,
+          dailyLimit:'',
           appName: 'ReelEarn Pro',
           version: '1.0.0',
           createdBy: user.uid,
@@ -1069,7 +1069,7 @@ const cashflowService = {
         endDate.setFullYear(today.getFullYear() + 1);
         
         const defaultCashflow = {
-          dailyLimit.Number(data.dailyLimt),
+          dailyLimit:'',
           todaySpent: 0,
           startDate: today.toISOString().split('T')[0],
           endDate: endDate.toISOString().split('T')[0],
@@ -1083,7 +1083,7 @@ const cashflowService = {
     } catch (error) {
       console.error('Error loading cashflow:', error);
       return {
-        dailyLimit: 0,
+        dailyLimit: '',
         todaySpent: 0,
         startDate: '',
         endDate: ''
